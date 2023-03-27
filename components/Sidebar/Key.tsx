41bca079da3ec3c30dc6a4fe3a9ff1dd1eb2ebdc
hyd <>
2023-03-27 09:50:39 +0800

    no message

--- components/Sidebar/Key.tsx
+++ components/Sidebar/Key.tsx
@@ -23,44 +23,44 @@ export const Key: FC<Props> = ({ apiKey, onApiKeyChange }) => {
     setIsChanging(false);
   };
 
-  // return isChanging ? (
-  //   <div className="flex transition-colors duration:200 hover:bg-gray-500/10 py-3 px-3 rounded-md cursor-pointer w-full items-center">
-  //     <IconKey size={16} />
+  return isChanging ? (
+    <div className="flex transition-colors duration:200 hover:bg-gray-500/10 py-3 px-3 rounded-md cursor-pointer w-full items-center">
+      <IconKey size={16} />
 
-  //     <input
-  //       className="ml-2 flex-1 h-[20px] bg-transparent border-b border-neutral-400 focus:border-neutral-100 text-left overflow-hidden overflow-ellipsis pr-1 outline-none text-white"
-  //       type="password"
-  //       value={newKey}
-  //       onChange={(e) => setNewKey(e.target.value)}
-  //       onKeyDown={handleEnterDown}
-  //     />
+      <input
+        className="ml-2 flex-1 h-[20px] bg-transparent border-b border-neutral-400 focus:border-neutral-100 text-left overflow-hidden overflow-ellipsis pr-1 outline-none text-white"
+        type="password"
+        value={newKey}
+        onChange={(e) => setNewKey(e.target.value)}
+        onKeyDown={handleEnterDown}
+      />
 
-  //     <div className="flex w-[40px]">
-  //       <IconCheck
-  //         className="ml-auto min-w-[20px] text-neutral-400 hover:text-neutral-100"
-  //         size={18}
-  //         onClick={(e) => {
-  //           e.stopPropagation();
-  //           handleUpdateKey(newKey);
-  //         }}
-  //       />
+      <div className="flex w-[40px]">
+        <IconCheck
+          className="ml-auto min-w-[20px] text-neutral-400 hover:text-neutral-100"
+          size={18}
+          onClick={(e) => {
+            e.stopPropagation();
+            handleUpdateKey(newKey);
+          }}
+        />
 
-  //       <IconX
-  //         className="ml-auto min-w-[20px] text-neutral-400 hover:text-neutral-100"
-  //         size={18}
-  //         onClick={(e) => {
-  //           e.stopPropagation();
-  //           setIsChanging(false);
-  //           setNewKey(apiKey);
-  //         }}
-  //       />
-  //     </div>
-  //   </div>
-  // ) : (
-  //   <SidebarButton
-  //     text="OpenAI API Key"
-  //     icon={<IconKey size={16} />}
-  //     onClick={() => setIsChanging(true)}
-  //   />
-  // );
+        <IconX
+          className="ml-auto min-w-[20px] text-neutral-400 hover:text-neutral-100"
+          size={18}
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsChanging(false);
+            setNewKey(apiKey);
+          }}
+        />
+      </div>
+    </div>
+  ) : (
+    <SidebarButton
+      text="OpenAI API"
+      icon={<IconKey size={16} />}
+      onClick={() => setIsChanging(true)}
+    />
+  );
 };
